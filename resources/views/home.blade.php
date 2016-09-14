3a55983f19abda2a5aa3c011cdf9e9647525b13e
Joe Anzalone <joe@shmit.com>
2016-09-14 02:06:51 -0400

    Create a really basic route, controller, and view with some basic styles

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>The Shmitcam</title>
+    <link rel="stylesheet" href="/styles/main.css">
+</head>
+<body>
+    <div class="page">
+        <img class="cam" src="{{ $stream_url }}" alt="live webcam image">
+    </div>
+</body>
+</html>
