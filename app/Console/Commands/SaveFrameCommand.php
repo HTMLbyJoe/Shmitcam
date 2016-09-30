ff6eb98e04166119f3fe13a11b639dc48481314c
Joe Anzalone <joe@shmit.com>
2016-09-30 02:28:00 -0400

    Create a GIF for every x frames taken

--- app/Console/Commands/SaveFrameCommand.php
+++ app/Console/Commands/SaveFrameCommand.php
@@ -27,6 +27,8 @@ class SaveFrameCommand extends Command {
     public function fire()
     {
         $ignore_offline = $this->input->getOption('ignore-offline');
+        $max_frames = $this->input->getOption('frames');
+        $delay = $this->input->getOption('delay');
 
         if (!env('WEBCAM_ONLINE') && !$ignore_offline) {
             $this->error('Webcam is offline. Set WEBCAM_ONLINE to `true` in .env or override with --ignore-offline=true');
@@ -36,15 +38,45 @@ class SaveFrameCommand extends Command {
         $latest = env('WEBCAM_IMAGE_PATH');
 
         $this->info('Taking a pic');
-        $dir = base_path('storage/pending-frames/');
 
-        if (!file_exists($dir)) {
-            mkdir($dir, 0755, true);
+        $images_dir = base_path('storage/images/');
+        $pending_dir = $images_dir . 'pending-frames/';
+
+        if (!file_exists($pending_dir)) {
+            mkdir($pending_dir, 0755, true);
         }
 
-        $filename = date('Y-m-d_H:i:s') . '.jpg';
+        $filename = date('Y-m-d_H-i-s') . '.jpg';
 
-        copy($latest, $dir . $filename);
+        copy($latest, $pending_dir . $filename);
+
+        $file_iterator = new \FilesystemIterator($pending_dir, \FilesystemIterator::SKIP_DOTS);
+        $count = iterator_count($file_iterator);
+
+        if ($count >= $max_frames) {
+            $animation = new \Imagick();
+            $animation->setFormat('GIF');
+
+            $frames = scandir($pending_dir);
+
+            foreach($frames as $filename) {
+                $extension = pathinfo($filename, PATHINFO_EXTENSION);
+
+                if ($filename[0] === '.' || $extension !== 'jpg') {
+                    continue;
+                }
+
+                $frame = new \Imagick($pending_dir . $filename);
+                $animation->addImage($frame);
+                $animation->setImageDelay($delay);
+                $animation->nextImage();
+            }
+
+            $animation->writeImages($pending_dir . 'animation.gif', true);
+
+            $completed_dir = $images_dir . date('Y-m-d_H-i-s');
+            rename($pending_dir, $completed_dir);
+        }
     }
 
     /**
@@ -56,6 +88,8 @@ class SaveFrameCommand extends Command {
     {
         return [
             ['ignore-offline', null, InputOption::VALUE_OPTIONAL, 'Do this even if the camera is offline', false],
+            ['frames', null, InputOption::VALUE_OPTIONAL, 'How many frames to use per GIF', 20],
+            ['delay', null, InputOption::VALUE_OPTIONAL, 'The amount of time expressed in \'ticks\' that each frame should be displayed for', 20],
         ];
     }
 
